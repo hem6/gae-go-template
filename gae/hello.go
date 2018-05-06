b9f9c257d1156971897d3ea62965c24d104afe96
hem6 <hem6@users.noreply.github.com>
2018-05-06 16:43:21 +0900

    Use init() instead of main()

--- gae/hello.go
+++ gae/hello.go
@@ -2,18 +2,15 @@
 // Use of this source code is governed by the Apache 2.0
 // license that can be found in the LICENSE file.
 
-package main
+package app
 
 import (
 	"fmt"
 	"net/http"
-
-	"google.golang.org/appengine"
 )
 
-func main() {
+func init() {
 	http.HandleFunc("/", handle)
-	appengine.Main()
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
